a38232c6e5bc4b9a9ea6b800cfb35cefb9a6d88a
Parimala Palapati <27270@telxsids.net>
2022-04-09 16:32:45 +0530

    project

--- src/components/Service/service.js
+++ src/components/Service/service.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import { getTokenNow } from '../../utils/useToken';
+import { SERVERURL } from '../../utils/constants';
 
 export const getResourceByEmployeeNumber = (employeeNumber) => {
 
@@ -26,4 +26,199 @@ export const getResourceByEmployeeNumber = (employeeNumber) => {
 }
 
 
+export  const getAllTimesheet=async (filter)=>{
+    let url='http://10.75.80.111:8423/billing/v1/admin/timesheet'
+    if(filter){
+        if(filter!=='ALL PROJECTS'){
+            url +=`?projectname=${filter}`
+        }
+        
+    }
+     const tokenNow = await `Bearer ${getTokenNow()}`;
+    console.log('token now is', tokenNow)
+    const response =await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': tokenNow
+        },
 
+    });
+    const data= await response.json();
+    return  data;
+       
+
+}
+export  const saveTimeSheetEntry=async (data)=>{
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+   console.log('token now is', tokenNow)
+   const response =await fetch("http://10.75.80.111:8423/billing/v1/admin/timesheet", {
+       method: 'POST',
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Authorization': tokenNow
+       },
+       body:JSON.stringify(data)
+   });
+   return  response.status;
+}
+
+export  const updateTimeSheetEntry=async (data,id)=>{
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+    let url=`http://10.75.80.111:8423/billing/v1/admin/timesheet/${id}`;
+    console.log('url getting here is',url);
+   console.log('token now is', tokenNow)
+   const response =await fetch(`http://10.75.80.111:8423/billing/v1/admin/timesheet/${id}`, {
+       method: 'PUT',
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Authorization': tokenNow
+       },
+       body:JSON.stringify(data)
+   });
+   console.log('update statussssssssssssssssssssssssssssssssssssssssssssssss',response);
+   return  response.status;
+}
+export  const getAllProjects=async ()=>{
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+   console.log('token now is', tokenNow)
+
+   const response =await fetch("http://10.75.80.111:8423/billing/v1/admin/project", {
+       method: 'GET',
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Authorization': tokenNow
+       },
+
+   });
+   const data= await response.json();
+   return  data;
+      
+      
+
+}
+
+export const getAllResources = async () => {
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+
+    const response = await fetch(`${SERVERURL}/billing/v1/admin/resource`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': tokenNow
+        },
+
+    });
+    const data = await response.json();
+    return data;
+
+
+}
+
+export const saveResource = async (data) => {
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+    console.log('token now is', tokenNow)
+
+    const response = await fetch(`${SERVERURL}/billing/v1/admin/resource`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': tokenNow
+        },
+        body: JSON.stringify(data)
+
+    });
+    return response.status;
+}
+
+export const updateResource = async (data, id) => {
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+    console.log('token now is', tokenNow)
+
+    const response = await fetch(`${SERVERURL}/billing/v1/admin/resource/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': tokenNow
+        },
+        body: JSON.stringify(data)
+
+    });
+    return response.status;
+}
+
+export const getBilling=async()=>{
+    let url='http://10.75.80.111:8423/billing/v1/admin/billing'
+     const tokenNow = await `Bearer ${getTokenNow()}`;
+    console.log('token now is', tokenNow)
+    const response =await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': tokenNow
+        },
+
+    });
+    const data= await response.json();
+    return  data;
+       
+}
+export  const updateBilling=async (data,id)=>{
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+    let url=`http://10.75.80.111:8423/billing/v1/admin/billing/12${id}`;
+    console.log('url getting here is',url);
+   console.log('token now is', tokenNow)
+   const response =await fetch(url, {
+       method: 'PUT',
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Authorization': tokenNow
+       },
+       body:JSON.stringify(data)
+   });
+   console.log('update statussssssssssssssssssssssssssssssssssssssssssssssss',response);
+   return  response.status;
+}
+
+export  const createBilling=async (data)=>{
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+   console.log('token now is', tokenNow)
+   const response =await fetch("http://10.75.80.111:8423/billing/v1/admin/billing", {
+       method: 'POST',
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Authorization': tokenNow
+       },
+       body:JSON.stringify(data)
+   });
+   return  response.status;
+}
+
+export  const getAllRoles=async ()=>{
+    const tokenNow = await `Bearer ${getTokenNow()}`;
+   console.log('token now is', tokenNow)
+
+   const response =await fetch("http://10.75.80.111:8423/billing/v1/admin/role", {
+       method: 'GET',
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Authorization': tokenNow
+       },
+
+   });
+   const data= await response.json();
+   return  data;
+      
+
+}
